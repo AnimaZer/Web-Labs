78d166605383b089ad5f43c82c76898584a4e4c3
AnimaZer <43175599+AnimaZer@users.noreply.github.com>
2019-10-15 17:00:18 +0300

    стилиииии!!!

--- Lab3/js/script.js
+++ Lab3/js/script.js
@@ -1,50 +1,35 @@
 window.onload = function () {
   var date = new Date();
   var dateDay = date.getDate();
-  var days = document.querySelectorAll(".calendar-day");
+  var days = document.querySelectorAll('.calendar-day');
   for (var i = 1; i <= days.length; i++) {
-    if (dateDay == i) days[i].classList.add("today");
+    if (dateDay == i) days[i].classList.add('today');
   }
 };
 
-function typicalDay() {
-  alert("This Is just a typical day");
-}
 
 function themeSelector() {
-  var calendarTheme = document.getElementById("themeSelector");
-  var calendarElements = document.getElementsByClassName("item");
-  if (calendarTheme.value == "Dark") {
+  var calendarTheme = document.getElementById('themeSelector');
+  var calendarElements = document.getElementsByClassName('calendar-item');
+  if (calendarTheme.value == 'Dark') {
     for (var i = 0; i < calendarElements.length; i++) {
-      calendarElements[i].classList.add("calendar-dark");
-      document
-        .getElementsByClassName("calendar")[0]
-        .classList.add("calendar-dark");
-      document
-        .getElementsByClassName("calendar-day")[0]
-        .classList.add("calendar-dark");
-      document
-        .getElementsByClassName("calendar-head")[0]
-        .classList.add("calendar-dark");
-      document
-        .getElementsByClassName("calendar-month")[0]
-        .classList.add("calendar-dark");
+      calendarElements[i].classList.add('calendar-dark');
+      document.getElementsByClassName('calendar')[0]
+        .classList.add('calendar-dark');
+      document.getElementsByClassName('calendar-day')[0]
+        .classList.add('calendar-dark');
+      document.getElementsByClassName('calendar-month')[0]
+        .classList.add('calendar-dark');
     }
   } else {
     for (var i = 0; i < calendarElements.length; i++) {
-      calendarElements[i].classList.remove("calendar-dark");
-      document
-        .getElementsByClassName("calendar")[0]
-        .classList.remove("calendar-dark");
-      document
-        .getElementsByClassName("calendar-day")[0]
-        .classList.remove("calendar-dark");
-      document
-        .getElementsByClassName("calendar-head")[0]
-        .classList.remove("calendar-dark");
-      document
-        .getElementsByClassName("calendar-month")[0]
-        .classList.remove("calendar-dark");
+      calendarElements[i].classList.remove('calendar-dark');
+      document.getElementsByClassName('calendar')[0]
+        .classList.remove('calendar-dark');
+      document.getElementsByClassName('calendar-day')[0]
+        .classList.remove('calendar-dark');
+      document.getElementsByClassName('calendar-month')[0]
+        .classList.remove('calendar-dark');
     }
   }
 }
