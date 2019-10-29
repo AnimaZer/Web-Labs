34085fba06568a9565fb89be4e43e95f7c4d194c
AnimaZer <43175599+AnimaZer@users.noreply.github.com>
2019-10-29 14:06:34 +0300

    ренейм индекса, -отступы

--- Lab4 (Total Commander)/js/script.js
+++ Lab4 (Total Commander)/js/script.js
@@ -1,29 +1,28 @@
 window.onload = function () {
-  var i = 0;
-  
+  var itemIndex = 0; 
   var selectedItems = document.querySelectorAll('.tc-items');
-  selectedItems[i].classList.add('set');
+  selectedItems[itemIndex].classList.add('set');
   this.document.onkeydown = function (e) {
     if (e.keyCode === 39) {
-      selectedItems[i].classList.remove('set');
-      i = 5;
-      selectedItems[i].classList.add('set');
+      selectedItems[itemIndex].classList.remove('set');
+      itemIndex = 5;
+      selectedItems[itemIndex].classList.add('set');
     } else if (e.keyCode === 37) {
-      selectedItems[i].classList.remove('set');
-      i = 0;
-      selectedItems[i].classList.add('set');
+      selectedItems[itemIndex].classList.remove('set');
+      itemIndex = 0;
+      selectedItems[itemIndex].classList.add('set');
     } else if (e.keyCode === 40) {
-      if (i != 8) {
-        selectedItems[i].classList.remove('set');
-        i++;
-        selectedItems[i].classList.add('set');
+      if (itemIndex != 8) {
+        selectedItems[itemIndex].classList.remove('set');
+        itemIndex++;
+        selectedItems[itemIndex].classList.add('set');
       }
 
     } else if (e.keyCode === 38) {
-      if (i != 0) {
-        selectedItems[i].classList.remove('set');
-        i--;
-        selectedItems[i].classList.add('set');
+      if (itemIndex != 0) {
+        selectedItems[itemIndex].classList.remove('set');
+        itemIndex--;
+        selectedItems[itemIndex].classList.add('set');
       }
 
     }
