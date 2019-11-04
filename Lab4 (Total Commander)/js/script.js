470f5d3f20fd394056a294f93e6d979c14da6d92
AnimaZer <43175599+AnimaZer@users.noreply.github.com>
2019-11-05 01:42:12 +0300

    Add files via upload
    Глобальная переменная завернута в объект, флекс в строку и в колонку

--- Lab4 (Total Commander)/js/script.js
+++ Lab4 (Total Commander)/js/script.js
@@ -1,28 +1,30 @@
 window.onload = function () {
-  var itemIndex = 0; 
+  var itemIndex = {
+    count: 0
+  };
   var selectedItems = document.querySelectorAll('.tc-items');
-  selectedItems[itemIndex].classList.add('set');
+  selectedItems[itemIndex.count].classList.add('set');
   this.document.onkeydown = function (e) {
     if (e.keyCode === 39) {
-      selectedItems[itemIndex].classList.remove('set');
-      itemIndex = 5;
-      selectedItems[itemIndex].classList.add('set');
+      selectedItems[itemIndex.count].classList.remove('set');
+      itemIndex.count = 5;
+      selectedItems[itemIndex.count].classList.add('set');
     } else if (e.keyCode === 37) {
-      selectedItems[itemIndex].classList.remove('set');
-      itemIndex = 0;
-      selectedItems[itemIndex].classList.add('set');
+      selectedItems[itemIndex.count].classList.remove('set');
+      itemIndex.count = 0;
+      selectedItems[itemIndex.count].classList.add('set');
     } else if (e.keyCode === 40) {
-      if (itemIndex != 8) {
-        selectedItems[itemIndex].classList.remove('set');
-        itemIndex++;
-        selectedItems[itemIndex].classList.add('set');
+      if (itemIndex.count != 8) {
+        selectedItems[itemIndex.count].classList.remove('set');
+        itemIndex.count++;
+        selectedItems[itemIndex.count].classList.add('set');
       }
 
     } else if (e.keyCode === 38) {
-      if (itemIndex != 0) {
-        selectedItems[itemIndex].classList.remove('set');
-        itemIndex--;
-        selectedItems[itemIndex].classList.add('set');
+      if (itemIndex.count != 0) {
+        selectedItems[itemIndex.count].classList.remove('set');
+        itemIndex.count--;
+        selectedItems[itemIndex.count].classList.add('set');
       }
 
     }
