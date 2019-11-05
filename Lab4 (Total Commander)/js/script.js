945f4f00c9b93c63b5d7d0ac9c2c4601d83182fe
AnimaZer <43175599+AnimaZer@users.noreply.github.com>
2019-11-05 17:30:57 +0300

    Add files via upload
    bug-fixed

--- Lab4 (Total Commander)/js/script.js
+++ Lab4 (Total Commander)/js/script.js
@@ -3,28 +3,28 @@ window.onload = function () {
     count: 0
   };
   var selectedItems = document.querySelectorAll('.total-commander-item');
-  selectedItems[itemIndex.count].classList.add('set');
+  selectedItems[itemIndex.count].classList.add('selected');
   this.document.onkeydown = function (e) {
     if (e.keyCode === 39) {
-      selectedItems[itemIndex.count].classList.remove('set');
+      selectedItems[itemIndex.count].classList.remove('selected');
       itemIndex.count = 5;
-      selectedItems[itemIndex.count].classList.add('set');
+      selectedItems[itemIndex.count].classList.add('selected');
     } else if (e.keyCode === 37) {
-      selectedItems[itemIndex.count].classList.remove('set');
+      selectedItems[itemIndex.count].classList.remove('selected');
       itemIndex.count = 0;
-      selectedItems[itemIndex.count].classList.add('set');
+      selectedItems[itemIndex.count].classList.add('selected');
     } else if (e.keyCode === 40) {
-      if (itemIndex.count != selectedItems.length-1) {
-        selectedItems[itemIndex.count].classList.remove('set');
+      if (itemIndex.count != selectedItems.length - 1) {
+        selectedItems[itemIndex.count].classList.remove('selected');
         itemIndex.count++;
-        selectedItems[itemIndex.count].classList.add('set');
+        selectedItems[itemIndex.count].classList.add('selected');
       }
 
     } else if (e.keyCode === 38) {
       if (itemIndex.count != 0) {
-        selectedItems[itemIndex.count].classList.remove('set');
+        selectedItems[itemIndex.count].classList.remove('selected');
         itemIndex.count--;
-        selectedItems[itemIndex.count].classList.add('set');
+        selectedItems[itemIndex.count].classList.add('selected');
       }
 
     }
