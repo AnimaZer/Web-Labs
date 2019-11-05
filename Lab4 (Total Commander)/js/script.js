c0eb9c79aa52f08be216cbd36dbfb11568234e0d
AnimaZer <43175599+AnimaZer@users.noreply.github.com>
2019-11-05 17:08:37 +0300

    Add files via upload
    исправлены названия переменных и data-id

--- Lab4 (Total Commander)/js/script.js
+++ Lab4 (Total Commander)/js/script.js
@@ -2,7 +2,7 @@ window.onload = function () {
   var itemIndex = {
     count: 0
   };
-  var selectedItems = document.querySelectorAll('.tc-items');
+  var selectedItems = document.querySelectorAll('.total-commander-item');
   selectedItems[itemIndex.count].classList.add('set');
   this.document.onkeydown = function (e) {
     if (e.keyCode === 39) {
@@ -14,7 +14,7 @@ window.onload = function () {
       itemIndex.count = 0;
       selectedItems[itemIndex.count].classList.add('set');
     } else if (e.keyCode === 40) {
-      if (itemIndex.count != 8) {
+      if (itemIndex.count != selectedItems.length-1) {
         selectedItems[itemIndex.count].classList.remove('set');
         itemIndex.count++;
         selectedItems[itemIndex.count].classList.add('set');
