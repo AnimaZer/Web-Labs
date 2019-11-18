3bb671801b118c002f5670e93abff3097274d3ee
AnimaZer <43175599+AnimaZer@users.noreply.github.com>
2019-11-19 01:21:26 +0300

    Add files via upload
    исправлена проверка на наличие массивов

--- Lab5/Task4/script.js
+++ Lab5/Task4/script.js
@@ -1,5 +1,5 @@
 function isTimeRangesIntersect(timeRange1, timeRange2) {
-  if (Array.isArray(timeRange1) || Array.isArray(timeRange2)){
+  if (Array.isArray(timeRange1) && Array.isArray(timeRange2)){
     if ((timeRange1[0] >= timeRange2[0] && timeRange1[0] <= timeRange2[1] ) 
       || (timeRange2[0] >= timeRange1[0] && timeRange2[0] <= timeRange1[1])) {
       return true;
