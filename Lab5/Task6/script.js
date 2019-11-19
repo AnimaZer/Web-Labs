e44c387247b5c2f99df6a8a7e323ad3da5296eca
AnimaZer <43175599+AnimaZer@users.noreply.github.com>
2019-11-19 23:21:47 +0300

    Add files via upload
    Заменяет все искомые слова

--- Lab5/Task6/script.js
+++ Lab5/Task6/script.js
@@ -1,11 +1,14 @@
 function replaceString(text, oldString, newString) {
-  if (typeof text === 'string' 
-    && typeof oldString === 'string' 
-    && typeof newString === 'string'
-    && text.indexOf(oldString) >= 0) {
-      return text = text.slice(0, text.indexOf(oldString)) + newString + text.slice(text.indexOf(oldString) + oldString.length, text.length);
+  if (typeof text === 'string' &&
+    typeof oldString === 'string' &&
+    typeof newString === 'string' &&
+    text.indexOf(oldString) >= 0) {
+      while (text.indexOf(oldString) != -1) {
+        text = text.replace(oldString, newString);
+      }
+    return text;
   }
   return false;
 }
-console.log(replaceString('My code is beautiful', 'beautiful', 'BAD')); //true
+console.log(replaceString('My code is beautiful beautiful BISTROF', 'beautiful', 'BAD')); //true
 console.log(replaceString('Made in Chine', 'Ilysha', 'China')); //false
