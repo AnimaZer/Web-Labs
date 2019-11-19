80d389d5207b52da5178fe70acc27f3690b90262
AnimaZer <43175599+AnimaZer@users.noreply.github.com>
2019-11-19 14:46:48 +0300

    Add files via upload

--- Lab5/Task7/script.js
+++ Lab5/Task7/script.js
@@ -14,8 +14,8 @@ function gameStart() {
       console.log(x, y);
     },
     spawn: function () {
-      this.currentCoordinates.x = Math.floor(Math.random() * 3);
-      this.currentCoordinates.y = Math.floor(Math.random() * 3);
+      this.currentCoordinates.x = Math.floor(Math.random() * 10);
+      this.currentCoordinates.y = Math.floor(Math.random() * 10);
       console.log(this.name + ' spawned in coordinates: (' + this.currentCoordinates.x + '; ' + this.currentCoordinates.y + ');');
     }
   };
@@ -31,8 +31,8 @@ function gameStart() {
       console.log(x, y);
     },
     spawn: function () {
-      this.currentCoordinates.x = Math.floor(Math.random() * 3);
-      this.currentCoordinates.y = Math.floor(Math.random() * 3);
+      this.currentCoordinates.x = Math.floor(Math.random() * 10);
+      this.currentCoordinates.y = Math.floor(Math.random() * 10);
       console.log(this.name + ' spawned in coordinates: (' + this.currentCoordinates.x + '; ' + this.currentCoordinates.y + ');');
     }
   };
@@ -45,13 +45,12 @@ function gameStart() {
   }
 
   setInterval(function () {
-    // todo: spawn 2 soldiers
     if (sovietTank.health > 0 && germanTank.health > 0) {
       var shotX = Math.floor(Math.random() * 10);
       var shotY = Math.floor(Math.random() * 10);
 
       sovietTank.shot(shotX, shotY);
-      if (shotX === germanTank.currentCoordinates.x ||
+      if (shotX === germanTank.currentCoordinates.x &&
         shotY === germanTank.currentCoordinates.y) {
         console.log('Есть пробитие!');
         germanTank.health -= 25;
@@ -61,7 +60,7 @@ function gameStart() {
       }
 
       germanTank.shot(shotX, shotY);
-      if (shotX === sovietTank.currentCoordinates.x ||
+      if (shotX === sovietTank.currentCoordinates.x &&
         shotY === sovietTank.currentCoordinates.y) {
         console.log('Есть пробитие!');
         sovietTank.health -= 25;
@@ -80,5 +79,5 @@ function gameStart() {
       console.log(germanTank.name + ' is loser');
       flag.value = false;
     }
-  }, 100);
+  }, 50);
 }
