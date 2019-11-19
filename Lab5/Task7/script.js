b9b82835f093a55e6d809b6aa14314a16827f0ae
AnimaZer <43175599+AnimaZer@users.noreply.github.com>
2019-11-19 08:35:53 +0300

    Add files via upload

---
+++ Lab5/Task7/script.js
@@ -0,0 +1,84 @@
+function gameStart() {
+  var flag = {
+    value: true
+  };
+
+  var sovietTank = {
+    name: 'Object-260',
+    health: 100,
+    currentCoordinates: {
+      x: 10,
+      y: 20
+    },
+    shot: function (x, y) {
+      console.log(x, y);
+    },
+    spawn: function () {
+      this.currentCoordinates.x = Math.floor(Math.random() * 3);
+      this.currentCoordinates.y = Math.floor(Math.random() * 3);
+      console.log(this.name + ' spawned in coordinates: (' + this.currentCoordinates.x + '; ' + this.currentCoordinates.y + ');');
+    }
+  };
+
+  var germanTank = {
+    name: 'Leopard',
+    health: 100,
+    currentCoordinates: {
+      x: 10,
+      y: 20
+    },
+    shot: function (x, y) {
+      console.log(x, y);
+    },
+    spawn: function () {
+      this.currentCoordinates.x = Math.floor(Math.random() * 3);
+      this.currentCoordinates.y = Math.floor(Math.random() * 3);
+      console.log(this.name + ' spawned in coordinates: (' + this.currentCoordinates.x + '; ' + this.currentCoordinates.y + ');');
+    }
+  };
+
+  sovietTank.spawn();
+  germanTank.spawn();
+  if (germanTank.currentCoordinates.x === sovietTank.currentCoordinates.x ||
+    germanTank.currentCoordinates.y === sovietTank.currentCoordinates.y) {
+    germanTank.spawn();
+  }
+
+  setInterval(function () {
+    // todo: spawn 2 soldiers
+    if (sovietTank.health > 0 && germanTank.health > 0) {
+      var shotX = Math.floor(Math.random() * 10);
+      var shotY = Math.floor(Math.random() * 10);
+
+      sovietTank.shot(shotX, shotY);
+      if (shotX === germanTank.currentCoordinates.x ||
+        shotY === germanTank.currentCoordinates.y) {
+        console.log('Есть пробитие!');
+        germanTank.health -= 25;
+        console.log('Здоровье ' + germanTank.name + ' = ' + germanTank.health + ' HP');
+      } else {
+        console.log('Броня не пробита');
+      }
+
+      germanTank.shot(shotX, shotY);
+      if (shotX === sovietTank.currentCoordinates.x ||
+        shotY === sovietTank.currentCoordinates.y) {
+        console.log('Есть пробитие!');
+        sovietTank.health -= 25;
+        console.log('Здоровье ' + sovietTank.name + ' = ' + sovietTank.health + ' HP');
+      } else {
+        console.log('Броня не пробита');
+      }
+    }
+
+    if (sovietTank.health === 0 && flag.value) {
+      console.log(germanTank.name + ' is champion!');
+      console.log(sovietTank.name + ' is loser');
+      flag.value = false;
+    } else if (germanTank.health === 0 && flag.value) {
+      console.log(sovietTank.name + ' is champion!');
+      console.log(germanTank.name + ' is loser');
+      flag.value = false;
+    }
+  }, 100);
+}
